020dc845f00a6ddf6c516c96461cd3be26199e11
sbasnet3 <sbasnet3@learn-ap2000.chtc.wisc.edu>
2026-04-25 22:21:15 -0500

    Integrate remote updates with local analysis scripts

--- src/analysis/data_summary.R
+++ src/analysis/data_summary.R
@@ -3,6 +3,7 @@ library(dplyr)
 args <- commandArgs(trailingOnly = TRUE)
 file <- args[1]
 
+<<<<<<< Updated upstream
 #local_file <- basename(file)
 df <- read.csv(file)
 
@@ -14,3 +15,11 @@ print(summary(df))
 #  summarise(mean_val = mean(some_column, na.rm = TRUE))
 
 #write.csv(result, paste0("result_", basename(file), ".csv"))
+=======
+file <- basename(file)
+df <- read.csv(file)
+
+cat("Hello")
+
+print(summary(df))
+>>>>>>> Stashed changes
--- src/analysis/data_summary.sh
+++ src/analysis/data_summary.sh
@@ -6,11 +6,13 @@ STAGING_PATH="/home/groups/STAT_DSCP/Group4_NYCTaxi/$FILENAME"
 echo $FILENAME
 echo $STAGING_PATH
 
-cp "$STAGING_PATH" "$FILENAME"
+#cp "$STAGING_PATH" "$FILENAME"
 
 if [ ! -s "$FILENAME" ]; then
     echo "ERROR: File copy from staging failed or file is empty"
     exit 1
 fi
 
-Rscript data_summary.R "$FILENAME"
+Rscript data_summary.R "$STAGING_PATH"
+
+#
